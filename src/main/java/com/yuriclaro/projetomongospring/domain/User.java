b606aa00e1a889e712cf1bd390ae6f17e7432458
Yuri Claro <yuri.claro@outlook.com>
2024-02-07 17:40:23 -0300

    Referenciando os posts do usuario

--- src/main/java/com/yuriclaro/projetomongospring/domain/User.java
+++ src/main/java/com/yuriclaro/projetomongospring/domain/User.java
@@ -1,8 +1,11 @@
 package com.yuriclaro.projetomongospring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -12,6 +15,16 @@ public class User implements Serializable{
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
     public User(){
     }
 
