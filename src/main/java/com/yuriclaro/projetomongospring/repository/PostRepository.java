482e2451b27eb11df486a1022cae16abd2edb9aa
Yuri Claro <yuri.claro@outlook.com>
2024-02-13 16:25:45 -0300

    Consulta simples com @Query

--- src/main/java/com/yuriclaro/projetomongospring/repository/PostRepository.java
+++ src/main/java/com/yuriclaro/projetomongospring/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.yuriclaro.projetomongospring.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.yuriclaro.projetomongospring.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	@Query("{ 'title': { $regex: ?0, $option: 'i' } }")
+	List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
