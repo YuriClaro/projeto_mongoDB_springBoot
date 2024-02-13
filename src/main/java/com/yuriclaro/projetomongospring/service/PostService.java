6fde58c5d8d613971c529c8f9aef84e46929bfef
Yuri Claro <yuri.claro@outlook.com>
2024-02-13 16:30:32 -0300

    Consulta com vários critérios

--- src/main/java/com/yuriclaro/projetomongospring/service/PostService.java
+++ src/main/java/com/yuriclaro/projetomongospring/service/PostService.java
@@ -2,6 +2,7 @@ package com.yuriclaro.projetomongospring.service;
 
 import java.util.Optional;
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +25,9 @@ public class PostService {
     public List<Post> findByTitle(String text) {
 		return postRepository.searchTitle(text);
     }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24  * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
 }
